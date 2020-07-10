6a88429e97e95bf25492f8bc7d144e841101be20
yukimurasawa <yuki.murasawa0826@gmail.com>
2020-07-10 23:01:20 +0900

    add admin user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_05_105254) do
+ActiveRecord::Schema.define(version: 2020_07_10_135645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.integer "tag"
+  create_table "admin_users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
