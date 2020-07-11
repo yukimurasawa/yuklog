07daf875fb053e575b10d6719c3cf94155ba522b
yukimurasawa <yuki.murasawa0826@gmail.com>
2020-07-11 20:19:53 +0900

    admin article index page stype

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module Yuklog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
     config.generators do |g|
       g.test_framework :rspec,
                        view_specs: false,
