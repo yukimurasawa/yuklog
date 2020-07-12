147576afff6befef1229c997473ebb5ab8539467
yukimurasawa <yuki.murasawa0826@gmail.com>
2020-07-12 13:32:50 +0900

    add delete function

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -9,12 +9,22 @@ class Admin::ArticlesController < ApplicationController
     redirect_to admin_article_path(@article)
   end
 
+  def destroy
+    if @article.destroy
+      redirect_to admin_articles_path, flash: { notice: "記事の削除に成功しました!" }
+    else
+      redirect_to admin_articles_path, flash: { error: "記事の削除に失敗しました!" }
+    end
+  end
+
   private
 
   def load_resource
     case params[:action].to_sym
     when :index
       @articles = Article.order(created_at: 'DESC')
+    when :destroy
+      @article = Article.find(params[:id])
     end
   end
 
